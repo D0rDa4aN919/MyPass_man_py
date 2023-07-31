7cecc14ed92609dfafeaef5f5104dd51ab665fc7
Dor <108623675+D0rDa4aN919@users.noreply.github.com>
2023-08-01 00:18:22 +0300

    Update First_Time.py

--- First_Time.py
+++ First_Time.py
@@ -1,3 +1,9 @@
+##############################################################################
+# Offline My_Password_Man - Python Script
+# Description: A GUI-based password manager tool that operates offline to securely store and manage your passwords and sensitive information. It can be run on USB for maximum security, by using the autorun function in Windows.
+# Author: Dor Dahan
+# License: MIT (See details in the LICENSE file or at the end of this script)
+##############################################################################
 import json
 import os.path
 from cryptography.hazmat.primitives import padding, hashes
@@ -186,3 +192,25 @@ def creating_sec():
 
 if __name__ == '__main__':
     creating_sec()
+
+# License Information
+# This script is open-source and released under the MIT License.
+# MIT License
+# Copyright (c) 2023 Dor Dahan
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# For more details, see the LICENSE file in the root directory of this repository
+# or visit https://github.com/D0rDa4aN919/MyPass_man_py.
